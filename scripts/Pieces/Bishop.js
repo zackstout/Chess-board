56779dfa856c836c64b8c20e83e014ce73cee100
Zack Stout <zackstout@gmail.com>
2018-04-11 23:37:34 -0500

    almost have bishop move functionality working

--- scripts/Pieces/Bishop.js
+++ scripts/Pieces/Bishop.js
@@ -10,21 +10,43 @@ class Bishop extends Piece {
   }
 
   getMoves() {
-    console.log(this.color, board);
+    // console.log(this.color, board);
     let possibleMoves = [];
     const ind = 8 * this.pos.x + this.pos.y;
-    console.log(ind);
+    // console.log(ind);
 
     // this is a strange idea but i'll run with it:
     // Yeah this is a bit more complicated than we thought:
     // Need to check if it's on the edge. And need to add *each* cell on the diagonal, until we're beyond the grid.
     // That's not even taking into account blocking. But maybe that won't be too hard in the end.
 
-    // One idea is that we don't have to use the flattened 64-array: we could use the nested 8-arrays and use our coordinate system to find patterns as well. 
-    for (let i=0; i < 64; i++) {
-      if (i == ind - 7 || i == ind - 9 || i == ind + 7 || i == ind + 9) {
-        possibleMoves.push(board[i]);
-      }
+    // One idea is that we don't have to use the flattened 64-array: we could use the nested 8-arrays and use our coordinate system to find patterns as well.
+    // for (let i=0; i < 64; i++) {
+    //   if (i == ind - 7 || i == ind - 9 || i == ind + 7 || i == ind + 9) {
+    //     possibleMoves.push(board[i]);
+    //   }
+    // }
+
+    let ind1 = ind - 7;
+    let ind2 = ind + 7;
+    let ind3 = ind - 9;
+    let ind4 = ind + 9;
+
+    while (ind1 > 0) {
+      possibleMoves.push(board[ind1]);
+      ind1 -= 7;
+    }
+    while (ind2 < 64) {
+      possibleMoves.push(board[ind2]);
+      ind2 += 7;
+    }
+    while (ind3 > 0) {
+      possibleMoves.push(board[ind3]);
+      ind3 -= 9;
+    }
+    while (ind4 < 64) {
+      possibleMoves.push(board[ind4]);
+      ind4 += 9;
     }
     console.log(possibleMoves);
   }
--- scripts/Pieces/Rook.js
+++ scripts/Pieces/Rook.js
@@ -9,6 +9,21 @@ class Rook extends Piece {
     };
   }
 
-  // Need to check it's not blocked. 
+
+  getMoves() {
+    console.log(this.color, board);
+    let possibleMoves = [];
+
+    for (let i=0; i < 7; i++) {
+
+    }
+
+    for (let j=0; j < 7; j++) {
+
+    }
+
+    console.log(possibleMoves);
+  }
+  // Need to check it's not blocked.
 
 }
