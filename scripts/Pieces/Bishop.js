1526c565a4eeb271a9d96bdd4e3f3e5acd2d5faa
Zack Stout <zackstout@gmail.com>
2018-04-12 23:41:18 -0500

    nice, figured out bishop possible moves

--- scripts/Pieces/Bishop.js
+++ scripts/Pieces/Bishop.js
@@ -12,7 +12,7 @@ class Bishop extends Piece {
   getMoves() {
     // console.log(this.color, board);
     let possibleMoves = [];
-    const ind = 8 * this.pos.x + this.pos.y;
+    let ind = 8 * this.pos.x + this.pos.y;
     // console.log(ind);
 
     // this is a strange idea but i'll run with it:
@@ -20,34 +20,44 @@ class Bishop extends Piece {
     // Need to check if it's on the edge. And need to add *each* cell on the diagonal, until we're beyond the grid.
     // That's not even taking into account blocking. But maybe that won't be too hard in the end.
 
-    // One idea is that we don't have to use the flattened 64-array: we could use the nested 8-arrays and use our coordinate system to find patterns as well.
-    // for (let i=0; i < 64; i++) {
-    //   if (i == ind - 7 || i == ind - 9 || i == ind + 7 || i == ind + 9) {
-    //     possibleMoves.push(board[i]);
-    //   }
-    // }
+    // moving SW:
+    const ind1 = ind - 7;
+    // 7 because that's the index of extrema:
+    let min1 = Math.min(this.pos.x, 7 - this.pos.y); // will we hit the W or S side first?
 
-    let ind1 = ind - 7;
-    let ind2 = ind + 7;
-    let ind3 = ind - 9;
-    let ind4 = ind + 9;
+    // moving SE:
+    const ind2 = ind - 9;
+    let min2 = Math.min(this.pos.x, this.pos.y);
 
-    while (ind1 > 0) {
-      possibleMoves.push(board[ind1]);
-      ind1 -= 7;
+    // moving NE:
+    const ind3 = ind + 7;
+    let min3 = Math.min(7 - this.pos.x, this.pos.y); // will we hit the N or E side first?
+
+    // moving NW:
+    const ind4 = ind + 9;
+    let min4 = Math.min(7 - this.pos.x, 7 - this.pos.y);
+
+    while (min1 > 0) {
+      ind -= 7;
+      possibleMoves.push(board[ind]);
+      min1--;
     }
-    while (ind2 < 64) {
-      possibleMoves.push(board[ind2]);
-      ind2 += 7;
+    while (min2 > 0) {
+      ind -= 9;
+      possibleMoves.push(board[ind]);
+      min2--;  
     }
-    while (ind3 > 0) {
-      possibleMoves.push(board[ind3]);
-      ind3 -= 9;
+    while (min3 > 0) {
+      ind += 7;
+      possibleMoves.push(board[ind]);
+      min3--;
     }
-    while (ind4 < 64) {
-      possibleMoves.push(board[ind4]);
-      ind4 += 9;
+    while (min4 > 0) {
+      ind += 9;
+      possibleMoves.push(board[ind]);
+      min4--;
     }
+
     console.log(possibleMoves);
   }
 
